9464b7cf5ace21f977037534dfc4d29e27ca93b8
Blueturtle <104430690+Blueturtle08@users.noreply.github.com>
2022-10-16 15:15:22 +0000

    last

--- Main.java
+++ Main.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     int num;
+    int[] arr = new int[100];
+    Scanner sc = new Scanner(System.in);
 
-    Scanner = 
+    System.out.print("숫자를 입력하세요 : ");
+      
+    num = sc.nextInt();
+
+    for(int i = 0;i < num; i++)
+      {
+        arr[i] = i + 1;
+        System.out.println(arr[i]);
+      }
   }
 }
